2685fcd542cdff923ea1886bbb197bc38dfef52d
Rrantisi <rantisireham19@gmail.com>
2023-09-08 07:17:01 -0400

    adds bootstrap classes

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import TodosPage from '../../pages/TodosPage/TodosPage';
 import CreateTodoPage from '../../pages/CreateTodoPage/CreateTodoPage';
@@ -8,13 +9,15 @@ import DeleteTodoPage from '../../pages/DeleteTodoPage/DeleteTodoPage';
 import HomePage from '../../pages/HomePage/HomePage';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <main className="App">
-      <NavBar />
+    <main className="App bg-dark p-5 text-light">
+      <NavBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/todos" element={<TodosPage />} />
-        <Route path="/todos/add" element={<CreateTodoPage />} />
+        <Route path="/todos" element={<TodosPage isMenuOpen={isMenuOpen}/>} />
+        <Route path="/todos/add" element={<CreateTodoPage/>} />
         <Route path="/todos/:id/edit" element={<EditTodoPage />} />
         <Route path="/todos/:id/delete" element={<DeleteTodoPage />} />
       </Routes>
