96187dced1b4beef2e0f2aa7a54508e4e447323e
Rrantisi <rantisireham19@gmail.com>
2023-09-08 07:17:54 -0400

    adds page content

--- src/pages/TodosPage/TodosPage.js
+++ src/pages/TodosPage/TodosPage.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Todo from '../../components/Todo/Todo';
+// import * as todosAPI from '../../utilities/todos-api';
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './TodosPage.css'
 
-export default function TodosPage() {
-    return (
-        <h2>Todos Page</h2>
-    )
+export default function TodosPage({isMenuOpen}) {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function getTodos() {
+    //   const todos = await todosAPI.getAll();
+      setTodos(todos);
+    }
+    getTodos();
+  }, []);
+
+  const todoList = todos.map((todo) => (
+    <Todo todo={todo} key={todo._id} />
+  ));
+
+//   const columnWidthClass = isMenuOpen ? 'col-lg-9 col-md-8' : 'col-lg-12 col-md-12';
+//   console.log('isMenuOpen prop in TodosPage:', isMenuOpen);
+
+  return (
+    <div className="container-fluid" style={{ marginTop: 80 }}> 
+        <div className="row"> 
+            <div className='col-md-12 col-lg-12 col-sm-12'> 
+                <div className="todospage-div"> 
+                    <h3 className="heading">Task Dashboard</h3>
+                    <table className="table table-bordered table-striped table-warning text-muted"> 
+                        <thead>
+                            <tr>
+                                <th>Description</th>
+                                <th>Responsible</th>
+                                <th>Priority</th>
+                                <th>Category</th>
+                            </tr>
+                        </thead>
+                        <tbody>{todoList}</tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
