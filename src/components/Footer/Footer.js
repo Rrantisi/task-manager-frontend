7c0140d1c0036656b2e9527c335605510a39cfa3
Rrantisi <rantisireham19@gmail.com>
2023-09-08 07:18:35 -0400

    adds footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="text-secondary p-3 text-center">
+      {/* Footer content */}
+      &copy; 2023 Task Manager
+    </footer>
+  );
+}
+
+export default Footer;
