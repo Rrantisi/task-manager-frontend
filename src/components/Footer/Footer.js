bbb462a1d9fec8238a31009f65e6f4e2ef522983
Rrantisi <rantisireham19@gmail.com>
2023-09-09 11:16:46 -0400

    updates footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="text-secondary p-3 text-center">
+    <footer className="text-secondary pt-5 text-center">
       {/* Footer content */}
       &copy; 2023 Task Manager
     </footer>
