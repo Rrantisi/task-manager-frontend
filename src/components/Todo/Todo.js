dd23bcda134e889621c57db4879484c8904ddd83
Rrantisi <rantisireham19@gmail.com>
2023-09-29 08:13:53 -0400

    updates styling

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,16 +1,19 @@
 import { Link } from 'react-router-dom';
+import './Todo.css'
 
 export default function Todo({ todo }){
     return (
         <tr>
-            <td className= {todo.todo_completed ? 'completed' : ''}>{todo.todo_description}</td>
-            <td className= {todo.todo_completed ? 'completed' : ''}>{todo.todo_responsible}</td>
-            <td className= {todo.todo_completed ? 'completed' : ''}>{todo.todo_priority}</td>
-            <td>
-                <Link to={todo._id + "/edit/"} state={{todo}}>Edit</Link>
+            <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_description}</td>
+            <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_responsible}</td>
+            <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_priority}</td>
+            <td id='custom-task' className='bg-transparent shadow text-white custom-border'>
+                {/* <Link to={todo._id + "/edit/"} state={{todo}}>Edit</Link> */}
             </td>
-            <td>
-                <Link to={todo._id + "/delete/"} state={{todo}}>Delete</Link>
+            <td className='bg-transparent shadow text-white custom-border'>
+                <Link to={todo._id + "/edit/"} state={{todo}} className='text-warning text-decoration-none'>Edit</Link>
+                &nbsp; | &nbsp;
+                <Link to={todo._id + "/delete/"} state={{todo}} className='text-warning text-decoration-none'>Delete</Link>
             </td>
         </tr>
     )
