e34d71809fe55381f991f90926954a20a465309f
Rrantisi <rantisireham19@gmail.com>
2023-10-19 09:53:15 -0400

    updates app name

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,7 +4,7 @@ import './HomePage.css'
 export default function HomePage() {
     return (
         <div className='text-animation' style={{ marginTop: 100 }}> 
-            <h2>Welcome to Task Manager</h2>
+            <h2>Welcome to track<span className='text-success'>.</span><span className='text-warning'>.</span><span className='text-danger'>.</span>it</h2>
         </div>
     )
 }
