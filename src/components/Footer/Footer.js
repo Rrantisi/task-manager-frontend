482c9ee04d23c4f95c5c66674dc18b577771cb9a
Rrantisi <rantisireham19@gmail.com>
2023-10-19 10:51:26 -0400

    updates font style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="text-secondary pt-5 text-center">
       {/* Footer content */}
-      &copy; 2023 track<span className='text-success'>.</span><span className='text-warning'>.</span><span className='text-danger'>.</span>it
+      &copy; 2023 <span className='logoFont logoBigger'>track<span className='text-success'>.</span><span className='text-warning'>.</span><span className='text-danger'>.</span>it</span>
     </footer>
   );
 }
