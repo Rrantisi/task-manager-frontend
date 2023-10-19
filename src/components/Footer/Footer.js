25c7ddce508508bb786cfb8b9669d1654e111bfd
Rrantisi <rantisireham19@gmail.com>
2023-10-19 09:52:56 -0400

    updates app name

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="text-secondary pt-5 text-center">
       {/* Footer content */}
-      &copy; 2023 Task Manager
+      &copy; 2023 track<span className='text-success'>.</span><span className='text-warning'>.</span><span className='text-danger'>.</span>it
     </footer>
   );
 }
