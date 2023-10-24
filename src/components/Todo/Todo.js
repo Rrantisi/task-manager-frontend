70c36fbb22afbf33a3b18c7c3107bd55f5cfcf26
Rrantisi <rantisireham19@gmail.com>
2023-10-24 08:05:45 -0400

    fixes indentation

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom';
 import './Todo.css'
 
 export default function Todo({ todo }){
-    return (
-        <tr>
-            <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_description}</td>
-            <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_responsible}</td>
-            <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_priority}</td>
-            <td className='bg-transparent shadow text-white custom-border'>
-                <Link to={todo._id + "/edit/"} state={{todo}} className='text-warning text-decoration-none'>Edit</Link>
-                &nbsp; | &nbsp;
-                <Link to={todo._id + "/delete/"} state={{todo}} className='text-warning text-decoration-none'>Delete</Link>
-            </td>
-        </tr>
-    )
+  return (
+      <tr>
+          <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_description}</td>
+          <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_responsible}</td>
+          <td id='custom-task' className= {`bg-transparent shadow text-white custom-border ${todo.todo_completed ? 'completed' : ''}`}>{todo.todo_priority}</td>
+          <td className='bg-transparent shadow text-white custom-border'>
+              <Link to={todo._id + "/edit/"} state={{todo}} className='text-warning text-decoration-none'>Edit</Link>
+              &nbsp; | &nbsp;
+              <Link to={todo._id + "/delete/"} state={{todo}} className='text-warning text-decoration-none'>Delete</Link>
+          </td>
+      </tr>
+  )
 }
