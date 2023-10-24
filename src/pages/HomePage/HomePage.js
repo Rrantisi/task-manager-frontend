6b1d9f4fde8f6b9f0a36c37ea9cb9c36455ace5d
Rrantisi <rantisireham19@gmail.com>
2023-10-24 08:16:54 -0400

    fixes indentation

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './HomePage.css'
 
 export default function HomePage() {
-    return (
-        <div className='text-animation' style={{ marginTop: 100 }}> 
-            <h2>Welcome to <span className='logoFont'><span className='text-success'>.</span><span className='text-warning'>.</span><span className='text-danger'>.</span>tracked</span></h2>
-        </div>
-    )
+  return (
+      <div className='text-animation' style={{ marginTop: 100 }}> 
+          <h2>Welcome to <span className='logoFont'><span className='text-success'>.</span><span className='text-warning'>.</span><span className='text-danger'>.</span>tracked</span></h2>
+      </div>
+  )
 }
