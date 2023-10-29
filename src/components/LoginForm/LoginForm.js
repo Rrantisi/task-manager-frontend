a9ad0d73612fcc3e962daa770fab4ab0a8c7f9a5
Rrantisi <rantisireham19@gmail.com>
2023-10-29 08:27:13 -0400

    updates LoginForm

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,39 +2,39 @@ import { useState } from 'react';
 import * as usersService from '../../utilities/users-service';
 
 export default function LoginForm({ setUser }) {
-    const [credentials, setCredentials] = useState({
-        email: '',
-        password: ''
-    })
-    const [error, setError] = useState('');
+  const [credentials, setCredentials] = useState({
+      email: '',
+      password: ''
+  })
+  const [error, setError] = useState('');
 
-    function handleChange(evt){
-        setCredentials({ ...credentials, [evt.target.name]: evt.target.value});
-        setError('');
-    }
+  function handleChange(evt){
+      setCredentials({ ...credentials, [evt.target.name]: evt.target.value});
+      setError('');
+  }
 
-    async function handleSubmit(evt){
-        evt.preventDefault();
-        try {
-            const user = await usersService.login(credentials);
-            setUser(user);
-        } catch {
-            setError('Log In Failed - Try Again');
-        }
-    }
+  async function handleSubmit(evt){
+      evt.preventDefault();
+      try {
+          const user = await usersService.login(credentials);
+          setUser(user);
+      } catch {
+          setError('Log In Failed - Try Again');
+      }
+  }
 
-    return (
-        <div>
-            <div className="form-container">
-                <form autoComplete="off" onSubmit={handleSubmit}>
-                    <label className="form-label">Email</label>
-                    <input type="text" name="email" value={credentials.email} onChange={handleChange} className="form-control" required />
-                    <label className="form-label">Password</label>
-                    <input type="password" name="password" value={credentials.password} onChange={handleChange} className="form-control" required />
-                    <button type="submit" className="btn btn-outline-warning mt-2">Log In</button>
-                </form>
-            </div>
-            <p className="text-danger">&nbsp;{error}</p>
-        </div>
-    )
+  return (
+      <div>
+          <div className="form-container">
+              <form autoComplete="off" onSubmit={handleSubmit}>
+                  <label className="form-label">Email</label>
+                  <input type="text" name="email" value={credentials.email} onChange={handleChange} className="form-control" required />
+                  <label className="form-label">Password</label>
+                  <input type="password" name="password" value={credentials.password} onChange={handleChange} className="form-control" required />
+                  <button type="submit" className="btn btn-outline-warning mt-2">Log In</button>
+              </form>
+          </div>
+          <p className="text-danger">&nbsp;{error}</p>
+      </div>
+  )
 }
